7bed18f5f2494c0895055ef2a19e17ba957c5291
TonyWhitley <Tony_Whitley@hotmail.com>
2019-06-26 14:07:22 +0100

    text2speech turned into a lib (More reg files to enable voices)

--- text2speech.py
+++ text2speech.py
@@ -1,36 +1,84 @@
+""" Text to speech using one of the standard Windows voices """
 import pyttsx3
-engine = pyttsx3.init('sapi5') # object creation
 
-""" RATE"""
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 150)     # setting up new voice rate
+class Text2Speech:
+    """ Text to speech using one of the standard Windows voices """
+    def __init__(self, verbose: bool = False):
+        self.engine = pyttsx3.init('sapi5') # object creation
+        self.verbose = verbose
 
+    def set_rate(self, rate: int = 150):
+        """ Speaking rate. Default is 200 """
+        self.engine.setProperty('rate', rate)       # setting up new voice rate
+        # (Takes a little while before property can be read back)
 
-"""VOLUME"""
-volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-print (volume)                          #printing current volume level
-engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+    def set_volume(self, volume: float = 1.0):
+        """ Volume. 0.0 to 1.0 """
+        if volume > 1.0:
+            volume = 1.0
+        if volume < 0.0:
+            volume = 0.0
+        self.engine.setProperty('volume', volume)    # setting up volume level  between 0 and 1
+        # (Takes a little while before property can be read back)
 
-"""VOICE"""
-voices = engine.getProperty('voices')       #getting details of current voice
-for voice in voices:
-    print(voice.name)
-    # Microsoft Hazel Desktop - English (Great Britain)
-    # Microsoft George - English (United Kingdom)      after registry hack, see
-    #                                       Documentation\GeorgeVoiceEnable.reg
-    #  https://www.ghacks.net/2018/08/11/unlock-all-windows-10-tts-voices-system-wide-to-get-more-of-them/
-    # Microsoft Zira Desktop - English (United States)
-engine.setProperty('voice', voices[0].id)  #changing index, changes voices. 0 for Hazel
-#engine.setProperty('voice', voices[1].id)  #changing index, changes voices. 1 for George
-#engine.setProperty('voice', voices[2].id)   #changing index, changes voices. 1 for Zira
+    def select_voice(self, voice_name: str = 'Hazel'):
+        """
+        Set the voice to be used
+        (Use Documentation/<name>VoiceEnable.reg
+        to add the other standard UK voices)
+        """
+        voices = self.engine.getProperty('voices')  #getting details of current voice
+        for voice in voices:
+            if voice_name in voice.name:
+                # Microsoft Hazel Desktop - English (Great Britain)
+                # Microsoft George - English (United Kingdom)      after registry hack, see
+                #                                       Documentation\GeorgeVoiceEnable.reg
+                #pylint: disable=line-too-long
+                #  https://www.ghacks.net/2018/08/11/unlock-all-windows-10-tts-voices-system-wide-to-get-more-of-them/
+                # Microsoft Zira Desktop - English (United States)
+                self.engine.setProperty('voice', voice.id)
+                if self.verbose:
+                    self.say(f'Voice set to, {voice_name:s}.')
+                return
+        # default to voice 0 - Hazel
+        self.engine.setProperty('voice', voices[0].id)
+        if self.verbose:
+            # Extract the voice name from "Microsoft Hazel Desktop - English (Great Britain)"
+            voice_name = voices[0].name.split()[1]
+            self.say(f'Voice set to default, {voice_name:s}.')
 
-#engine.say("Hello World!")
-rate = engine.getProperty('rate') # always reports 200 even though rate changes
-#engine.say('My current speaking rate is ' + str(rate))
+    def get_volume(self):
+        """ Volume """
+        _vol = self.engine.getProperty('volume')
+        if self.verbose:
+            self.say(f'Volume is, {_vol:.1f}.')
 
-for program in ['r Factor', 'Crew Chief', 'Discord', 'Gear shift', '10%', '50%', 'Full volume', 'Seven Smiles', 'Wrighty']:
-    engine.say(program)
-engine.runAndWait()
-engine.stop()
+    def get_rate(self):
+        """ Speech rate """
+        _rate = self.engine.getProperty('rate')
+        if self.verbose:
+            self.say(f'Speech rate is, {_rate:d}.')
 
+    def say(self, phrase: str):
+        """
+        Say the phrase.
+        . and , introduce a pause.
+        """
+        self.engine.say(phrase)
+        self.engine.runAndWait()
+
+def main():
+    """ demo """
+    tts_o = Text2Speech(verbose=True)
+    tts_o.set_rate(180)
+    tts_o.set_volume(0.9)
+    tts_o.select_voice('George')
+    tts_o.get_volume()
+    tts_o.get_rate()
+
+    for phrase in ['r Factor', 'Crew Chief', 'Discord', 'Gear shift', \
+                    '10%', '50%', 'Full volume', 'Seven Smiles', 'Wrighty']:
+        tts_o.say(phrase)
+
+if __name__ == "__main__":
+    main()
